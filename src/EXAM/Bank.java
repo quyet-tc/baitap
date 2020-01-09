739bd312abe9cc1f079af4d16d44dad2f8fff412
quyet-tc <53265406+quyet-tc@users.noreply.github.com>
2020-01-08 23:42:24 -0800

    first commit

--- src/EXAM/Bank.java
+++ src/EXAM/Bank.java
@@ -1,46 +1,4 @@
-package EXAM;
 
-import java.util.Scanner;
-
-/**
- *
- * @author caube
- */
-public class bankhihi {
-   private int number;
-   private String destination;
-
-    public bankhihi(int number, String destination) {
-        this.number = number;
-        this.destination = destination;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-    public void display() {
-        System.out.println(number + "," + destination);
-    }
-
-    public static void main(String[] args) {
-        bankhihi g1 = new bankhihi(857, "Toronto");
-        System.out.println("Output:");
-        g1.display();
-    }
- 
-}
 
 package EXAM;
 
